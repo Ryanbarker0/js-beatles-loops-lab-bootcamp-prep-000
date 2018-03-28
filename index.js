6e9d41072f4bd6f6ab1e69b8f63bf81770a2803f
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-03-28 22:16:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var instrumentsPlayed = []
   for (var i = 0; i < musicians.length; i++) {
-    console.log(instrumentsPlayed[musicians +'plays'+instruments])
+    console.log(instrumentsPlayed[musicians[i] +'plays'+instruments[i]])
   }
 }
